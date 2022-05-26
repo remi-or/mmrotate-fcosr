569dc135c261fd8c8ae502a10984f19f353a6597
remi-or <remi.ouazan--reboul@mines-paristech.fr>
2022-05-26 18:28:24 -0400

    Fixes in dotafv

--- mmrotate/datasets/dotafv.py
+++ mmrotate/datasets/dotafv.py
@@ -1,33 +1,44 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 import os
-from itertools import chain, repeat
-import numpy as np
+from typing import List, Tuple
 
+import torch
 from mmcv.ops import box_iou_rotated
+from torch import Tensor
+
 from mmrotate.core import eval_rbbox_map
 from .builder import ROTATED_DATASETS
 from .dota import DOTADataset
 
 
-def format_dets(det_results):
-    return [
-        np.vstack([
-            np.hstack((bboxes, np.full(bboxes.shape[:, 0].shape, label)))
-            for label, bboxes in enumerate(image_dets)
-        ])
-        for image_dets in det_results
-    ]
+def format_dets(det_results) -> List[Tensor]:
+    acc = []
+    for image_dets in det_results:
+        image_acc = []
+        for label, bboxes in enumerate(image_dets):
+            image_acc.append(
+                torch.hstack(
+                    (bboxes[:, :-1], torch.full((bboxes.size(0), 1), label))))
+        acc.append(torch.vstack(image_acc).float())
+    return acc
 
-def format_gts(annotations):
-    return [
-        np.hstack((image_gts['bboxes'], image_gts['labels'].reshape(-1, 1)))
-        for image_gts in annotations
-    ]
 
-def eval_matches_single(dets, gts, iou_thr):
+def format_gts(annotations) -> List[Tensor]:
+    acc = []
+    for image_gts in annotations:
+        gts = torch.hstack((
+            torch.tensor(image_gts['bboxes']),
+            torch.tensor(image_gts['labels']),
+        ))
+        acc.append(gts.float())
+    return acc
+
+
+def eval_matches_single(dets, gts, iou_thr) -> Tuple[int, ...]:
     true_matches, false_matches, false_dets = 0, 0, 0
     missed_gts = set(range(len(gts)))
-    ious = box_iou_rotated(dets[:, :-1], gts[:, :-1])
-    for i_det, i_gt in enumerate(np.argmax(ious, 1)):
+    ious: Tensor = box_iou_rotated(dets[:, :-1], gts[:, :-1])
+    for i_det, i_gt in enumerate(ious.argmax(1).tolist()):
         if ious[i_det, i_gt] >= iou_thr:
             if i_gt in missed_gts:
                 missed_gts.remove(i_gt)
@@ -37,13 +48,15 @@ def eval_matches_single(dets, gts, iou_thr):
                 false_matches += 1
         else:
             false_dets += 1
-    return true_matches, false_matches, false_dets, missed_gts
+    return true_matches, false_matches, false_dets, len(missed_gts)
 
-def eval_matches(det_results, annotations, iou_thr=0.5):
-    accumulator = np.zeros((4,)) # true_matches, false_matches, false_dets, missed_gts
+
+def eval_matches(det_results, annotations, iou_thr=0.5) -> List[float]:
+    accumulator = torch.zeros(
+        (4, ))  # true_matches, false_matches, false_dets, missed_gts
     for dets, gts in zip(format_dets(det_results), format_gts(annotations)):
-        accumulator += np.array(eval_matches_single(dets, gts, iou_thr))
-    return accumulator / np.sum(accumulator)
+        accumulator += torch.tensor(eval_matches_single(dets, gts, iou_thr))
+    return (accumulator / accumulator.sum()).tolist()
 
 
 @ROTATED_DATASETS.register_module()
@@ -88,7 +101,5 @@ class DOTAFullValDataset(DOTADataset):
             nproc=nproc)
         eval_results['mAP'] = mean_ap
         # matches
-        eval_results['matches'] = eval_matches(results, 
-                                               annotations, 
-                                               iou_thr)
+        eval_results['matches'] = eval_matches(results, annotations, iou_thr)
         return eval_results
